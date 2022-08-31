9c214c6b58edf82bf77f2d57ad61d88f82c7d1ea
Marcelo-MV <marcelomventura@hotmail.com>
2022-08-31 12:17:13 -0300

    base de imagens

--- xhr.js
+++ xhr.js
@@ -2,18 +2,23 @@ let xhr = new XMLHttpRequest()
 let docHTML = document.getElementById('teste')
 xhr.open("GET", "/dataset.xml", true )
 xhr.send()
-xhr.onreadystatechange = ()=>{
+xhr.onreadystatechange = function () {
     if(xhr.readyState == 4 && xhr.status == 200){
-        x = xhr.responseXML.getElementsByTagName("dog")[0]
-        y = x.getElementsByTagName("raca")[0].textContent
-        img = x.getElementsByTagName ("foto")[0].textContent
-        docHTML.innerHTML = `<p> ${y} </p>
-        <img src="${img}">
-        `
-
-        
-        
-        console.log(xhr)    
+        render ()
   }
+  
+  
 }
 
+render = ()=> {
+x = xhr.responseXML.getElementsByTagName("dog")[2]
+y = x.getElementsByTagName("raca")[0].textContent
+img = x.getElementsByTagName ("foto")[0].textContent
+docHTML.innerHTML = `<p> ${y} </p>
+<img src="${img}">
+`
+
+
+
+console.log(x)
+}
